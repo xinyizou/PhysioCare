c1d2aea28f1a7fb851143d179786434e15e3e36c
xinyizou <43503199+xinyizou@users.noreply.github.com>
2019-09-14 12:50:35 -0400

    prototype layout, pre androidx migration

---
+++ app/src/main/java/com/example/hackthenorth2019_android/ChatActivity.kt
@@ -0,0 +1,17 @@
+package com.example.hackthenorth2019_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class ChatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+
+
+    }
+
+
+}
--- app/src/main/java/com/example/hackthenorth2019_android/MainActivity.kt
+++ app/src/main/java/com/example/hackthenorth2019_android/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.example.hackthenorth2019_android
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rehab.setOnClickListener {
+            goToRehabActivity()
+        }
+
+        chronic.setOnClickListener {
+            goToChronicActivity()
+        }
+
+        chat.setOnClickListener {
+            goToChatActivity()
+        }
     }
+
+    fun goToRehabActivity() {
+        val intent = Intent(this, RehabActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goToChronicActivity() {
+        val intent = Intent(this, ChronicActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goToChatActivity() {
+        val intent = Intent(this, ChatActivity::class.java)
+        startActivity(intent)
+    }
+
 }
