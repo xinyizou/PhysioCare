f4b1cc81714577c8318b82018c0871e677a92e47
xinyizou <43503199+xinyizou@users.noreply.github.com>
2019-09-14 12:57:51 -0400

    successful androidx migration, login ui blank

--- app/src/main/java/com/example/hackthenorth2019_android/ChronicActivity.kt
+++ app/src/main/java/com/example/hackthenorth2019_android/ChronicActivity.kt
@@ -1,6 +1,6 @@
 package com.example.hackthenorth2019_android
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
--- app/src/main/java/com/example/hackthenorth2019_android/MainActivity.kt
+++ app/src/main/java/com/example/hackthenorth2019_android/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.hackthenorth2019_android
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
