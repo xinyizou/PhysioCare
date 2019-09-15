92ada0dbcd1e579c2d6a75469ec5bd4c4241c123
xinyizou <43503199+xinyizou@users.noreply.github.com>
2019-09-14 22:00:09 -0400

    changing to graph views

--- app/src/main/java/com/example/hackthenorth2019_android/ChronicActivity.kt
+++ app/src/main/java/com/example/hackthenorth2019_android/ChronicActivity.kt
@@ -3,13 +3,18 @@ package com.example.hackthenorth2019_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_chronic.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.HashMap
 
 class ChronicActivity : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+    var muscledataset: Queue<Any> = ArrayDeque<Any>(10)
+    var degreedataset: Queue<Any> = ArrayDeque<Any>(10)
+    var ohmdataset: Queue<Any> = ArrayDeque<Any>(10)
 
     companion object {
         private const val TAG = "KotlinActivity"
@@ -18,19 +23,23 @@ class ChronicActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chronic)
+        database = FirebaseDatabase.getInstance().reference
+        readFB()
 
     }
 
     fun readFB() {
 
         val database = FirebaseDatabase.getInstance()
-        val myRef = database.getReference()
+        val myRef = database.getReference("Subset1")
         myRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                val value = dataSnapshot.getValue(String::class.java)
-                Log.d(TAG, "Value is: $value")
+                val dataset = dataSnapshot.getValue(object : GenericTypeIndicator<HashMap<String, Any>?>(){})
+
+                Log.d(TAG, "Value is: $dataset")
+
+                parseData(dataset)
+
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -39,5 +48,25 @@ class ChronicActivity : AppCompatActivity() {
             }
         })
         // [END read_message]
+    }
+
+    fun parseData(dataset : HashMap<String, Any>?) {
+
+        var muscle = dataset?.getValue("Muscle")
+        tensionvalue.text = muscle.toString().subSequence(0, 5)
+        muscledataset.add(muscle)
+        Log.d(TAG, "musclesaet is: $muscledataset")
+
+        var degree = dataset?.getValue("Degree")
+        posturedegrees.text = degree.toString()
+        degreedataset.add(degree)
+        Log.d(TAG, "musclesaet is: $degreedataset")
+
+        var resistance = dataset?.getValue("Resistance")
+        postureohm.text = resistance.toString().format("%.2f")
+        ohmdataset.add(resistance)
+        Log.d(TAG, "musclesaet is: $muscledataset")
+//        var message = dataset?.getValue("Message")
+//        var pressure = dataset?.getValue("Pressure")
     }
 }
