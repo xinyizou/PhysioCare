94c8e7755ce3540a8b35c80fce264557814257ea
xinyizou <43503199+xinyizou@users.noreply.github.com>
2019-09-14 22:41:09 -0400

    changing to graph views 2

--- app/src/main/java/com/example/hackthenorth2019_android/ChronicActivity.kt
+++ app/src/main/java/com/example/hackthenorth2019_android/ChronicActivity.kt
@@ -25,7 +25,6 @@ class ChronicActivity : AppCompatActivity() {
         setContentView(R.layout.activity_chronic)
         database = FirebaseDatabase.getInstance().reference
         readFB()
-
     }
 
     fun readFB() {
@@ -53,20 +52,95 @@ class ChronicActivity : AppCompatActivity() {
     fun parseData(dataset : HashMap<String, Any>?) {
 
         var muscle = dataset?.getValue("Muscle")
-        tensionvalue.text = muscle.toString().subSequence(0, 5)
+//        tensionvalue.text = muscle.toString().subSequence(0, 5)
         muscledataset.add(muscle)
+
+        var itr = muscledataset.iterator()
+        var index = 0
+
+        // hasNext() returns true if the queue has more elements
+        while (itr.hasNext()) {
+            // next() returns the next element in the iteration
+            if (index == 0) {
+                bar0.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+            if (index == 1) {
+                bar1.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+            if (index == 2) {
+                bar2.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+            if (index == 3) {
+                bar3.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+            if (index == 4) {
+                bar4.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+            if (index == 5) {
+                bar5.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+            if (index == 6) {
+                bar6.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+            if (index == 7) {
+                bar7.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+            if (index == 8) {
+                bar8.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+            if (index == 9) {
+                bar9.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+
+            index++
+
+        }
         Log.d(TAG, "musclesaet is: $muscledataset")
 
         var degree = dataset?.getValue("Degree")
-        posturedegrees.text = degree.toString()
+//        posturedegrees.text = degree.toString()
         degreedataset.add(degree)
+        itr = degreedataset.iterator()
+        index = 0
+
+        // hasNext() returns true if the queue has more elements
+        while (itr.hasNext()) {
+            // next() returns the next element in the iteration
+            if (index == 0) {
+                pbar0.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+            if (index == 1) {
+                pbar1.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+            if (index == 2) {
+                pbar2.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+            if (index == 3) {
+                pbar3.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+            if (index == 4) {
+                pbar4.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+            if (index == 5) {
+                pbar5.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+            if (index == 6) {
+                pbar6.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+            if (index == 7) {
+                pbar7.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+            if (index == 8) {
+                pbar8.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+            if (index == 9) {
+                pbar9.layoutParams.height = itr.next().toString().toInt() / 5 * 300
+            }
+
+            index++
+
+        }
         Log.d(TAG, "musclesaet is: $degreedataset")
 
-        var resistance = dataset?.getValue("Resistance")
-        postureohm.text = resistance.toString().format("%.2f")
-        ohmdataset.add(resistance)
-        Log.d(TAG, "musclesaet is: $muscledataset")
-//        var message = dataset?.getValue("Message")
-//        var pressure = dataset?.getValue("Pressure")
     }
 }
